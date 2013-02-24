1e1fb56e371f0a5ddd5f253c97a33fe69101109c
peeter@peeter-NB.lan <peeter@peeter-NB.lan>
2013-02-24 09:02:42 +0200

    CaseCatcher tool added to project. Sensors message added to PC module. SensorBoard code for 4 URF sensors. Pins not configured.

--- Software/LowLevel/SensorBoard/inc/Sensor.h
+++ Software/LowLevel/SensorBoard/inc/Sensor.h
@@ -52,7 +52,11 @@ extern void Sensor_init(void);
 
 extern void Sensor_TASK_startMeasurement1(void);
 extern void Sensor_TASK_startMeasurement2(void);
+extern void Sensor_TASK_startMeasurement3(void);
+extern void Sensor_TASK_startMeasurement4(void);
 extern void Sensor_TASK_readDistance1(void);
 extern void Sensor_TASK_readDistance2(void);
+extern void Sensor_TASK_readDistance3(void);
+extern void Sensor_TASK_readDistance4(void);
 
 #endif /*LED_H_*/
