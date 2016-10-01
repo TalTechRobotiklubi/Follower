4053417816dd4d3d3037a98960d56e5ce094e8de
Peeter Salong <peetersalong@gmail.com>
2016-10-02 01:16:28 +0300

    Made tracker software to compile in windows without Kinect.

--- Software/Tracker/src/core.h
+++ Software/Tracker/src/core.h
@@ -10,11 +10,12 @@ struct core {
   CommOutput out_data;
   SerialComm serial;
   std::thread kinect_frame_thread;
-  std::atomic_bool running = ATOMIC_VAR_INIT(true);
+  std::atomic_bool running;
   struct kinect_frame* current_frame = nullptr;
   struct kinect_frame_source* frame_source = nullptr;
   struct UdpHost* udp = nullptr;
 
+  core() { running = ATOMIC_VAR_INIT(true); }
   ~core();
 };
 
