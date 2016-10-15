820c882d049538d0af9923279d81e606a729c709
Siim Kallas <siimkallas@gmail.com>
2016-10-15 15:33:04 +0300

    Fix texture creation.

--- Software/Tracker/src/Texture.cpp
+++ Software/Tracker/src/Texture.cpp
@@ -11,7 +11,7 @@ Texture TextureAllocate(int width, int height) {
   glBindTexture(GL_TEXTURE_2D, t.handle);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  glTexImage2D(GL_TEXTURE_2D, 1, GL_RGBA8, width, height, 0, GL_BGRA,
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_BGRA,
                GL_UNSIGNED_BYTE, NULL);
 
   return t;
@@ -31,7 +31,7 @@ void TextureUpdate(Texture* texture, const uint8_t* data, int width,
   if (texture->width != width || texture->height != height) {
     texture->width = width;
     texture->height = height;
-    glTexImage2D(GL_TEXTURE_2D, 1, GL_RGBA8, width, height, 0, GL_BGRA,
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_BGRA,
                  GL_UNSIGNED_BYTE, NULL);
 
   } else {
